6facdefb3ebe0761dd16c17cc9c65d128db15b31
TechFox6905 <prathamesh.gharat07@gmail.com>
2024-07-17 14:58:28 +0530

    Add pseudo code

--- Projects/TaskManager/TaskManager.py
+++ Projects/TaskManager/TaskManager.py
@@ -1,8 +1,36 @@
 #Task Class
-'''Defines the sturcture and behavior of task'''
+'''Defines the sturcture and behavior of task
+Task's Properties-
+1)Title
+2)Description
+3)priority
+4)category
+'''
 #Task Manager Class
-'''Manages a list of task with features'''
+'''Manages a list of task with features
+function of each -
+1)add task 
+2)delete task
+3)update task
+4)list all task
+5)list completed task
+6)list pending task'''
 #File I/O FUNCTION
-'''Save and load tasks to and form file'''
+'''Save and load tasks to and form file
+file type - JSON
+save file function 
+load file function
+'''
 #MAIN FUNCTION
-'''Display menu for user interaction'''
+'''Display menu for user interaction
+menu - 
+1.add task
+2.update task
+3.delete task
+4.list all task
+5.list completed
+6.list pending
+7.save tasks to file
+8.load task from file
+9.exit
+'''
