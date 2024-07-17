30ac19670c4e0fdc373dcfe36519f935ea2643d8
TechFox6905 <prathamesh.gharat07@gmail.com>
2024-07-17 21:36:11 +0530

    Added Task class and TaskManager class

--- Projects/TaskManager/TaskManager.py
+++ Projects/TaskManager/TaskManager.py
@@ -6,6 +6,21 @@ Task's Properties-
 3)priority
 4)category
 '''
+class Task:
+    def __init__(self, title, description, priority, category):
+        self.title = title
+        self.description = description
+        self.priority = priority
+        self.category = category
+        self.completed = False
+
+    def completed(self):
+        self.completed = True
+
+    def incompleted(self):
+        self.completed = False
+
+
 #Task Manager Class
 '''Manages a list of task with features
 function of each -
@@ -15,6 +30,26 @@ function of each -
 4)list all task
 5)list completed task
 6)list pending task'''
+
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+    
+    def add_task(self, task):
+        self.tasks.append(task)
+    
+    def update_task(self, index, task):
+        self.tasks[index] = task
+    
+    def delete_task(self,index):
+        self.tasks.pop(index)
+    
+    def list_tasks(self, filter_func =  None):
+        if filter_func is not None:
+            return list(filter(filter_func, self.tasks))
+        return self.tasks
+
+
 #File I/O FUNCTION
 '''Save and load tasks to and form file
 file type - JSON
